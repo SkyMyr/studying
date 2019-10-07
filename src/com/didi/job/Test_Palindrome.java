202964375a7f286ffb9a4eff0f0b9da7654f8ef0
unknown <244642897@qq.com>
2019-10-07 18:58:29 +0800

    107

--- src/com/didi/job/Test_Palindrome.java
+++ src/com/didi/job/Test_Palindrome.java
@@ -1,5 +1,7 @@
 package com.didi.job;
 
+import  java.util.*;
+import  java.lang.*;
 /**
  * @ClassNameTest_Palindrome
  * @Description 回文系列
@@ -27,7 +29,7 @@ public class Test_Palindrome {
                 low = low.next;
                 fast = fast.next.next;
             }
-            //反转链表
+//            反转前半部分链表
             ListNode pre = null;
             ListNode next = null;
             while(head != low){
@@ -36,9 +38,11 @@ public class Test_Palindrome {
                 pre = head;
                 head = next;
             }
+//            判断奇偶
             if(fast != null){
                 low = low.next;
             }
+//            回文比较
             while(low != null){
                 if(low.val != pre.val)
                     return false;
@@ -46,6 +50,41 @@ public class Test_Palindrome {
                 pre = pre.next;
             }
             return true;
+        }
+
+        public int primePalindrome(int N)
+        {
+            while(true){
+                String num = String.valueOf(N);
+                if( ( num.length() & 1 ) == 0)
+                    N = (int)Math.pow(10, num.length() + 1);
+                if(isPrime(N) && isReverse(N))
+                    return N;
+                N++;
+                /*if(N > 10000000 && N < 100000000)
+                    N = 100000000;*/
+            }
+        }
+
+        public boolean isPrime(int n)
+        {
+            if(n < 2)
+                return false;
+            for(int i = 2; i <= Math.sqrt(n); i++){
+                if(n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        public boolean isReverse(int n)
+        {
+            int result = 0, flag = n;
+            while(n > 0){
+                result = result * 10 + ( n % 10 );
+                n /= 10;
+            }
+            return flag == result? true : false;
         }
 
 }
